cee665c0d7eaac8c375b103c16ec50010d61ced7
newspicemedia <44853455+newspicemedia@users.noreply.github.com>
2018-11-07 21:40:08 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'pgcmobile',
+		wp_ws_url : 'http://m.peoplesgospelchurch.org/wp-appkit-api/pgcmobile',
+		wp_url : 'http://m.peoplesgospelchurch.org',
+		theme : 'q-android',
+		version : '',
+		app_title : 'PGC Mobile',
+		app_platform : 'ios',
+		gmt_offset : -5,
+		debug_mode : 'off',
+		auth_key : 'bt-c?jVUAFNTqnoAz1f)D3).K@Fu.7t8y5789i/5`w->-6EH C$ePs+}- edK#I~pA|2}IGh>',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
